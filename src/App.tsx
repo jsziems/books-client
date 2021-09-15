60072aaf332e48ad8078831936b140dfddd5da42
Janet Ziems <jsziems@gmail.com>
2021-09-14 21:22:27 -0400

    Initial push for client code,including typescript and material-ui installs.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Header } from './common'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      test
+    </div>
+  );
+}
+
+export default App;
