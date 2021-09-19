10cbae158996e80e4c751bafcaa5d00cb49973a2
Janet Ziems <jsziems@gmail.com>
2021-09-19 17:50:45 -0400

    Added basic functionality for adding a resource.

--- src/resources/ResourceCreate.tsx
+++ src/resources/ResourceCreate.tsx
@@ -1,3 +1,4 @@
+import { totalmem } from 'os'
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
 import { setSyntheticLeadingComments } from 'typescript'
@@ -8,7 +9,14 @@ type Props = {
 }
 
 export interface ResourceCreateState {
-    title: string
+    title: string,
+    author: string,
+    link: string,
+    topic: string,
+    media: string,
+    readStatus: string,
+    summary: string,
+    rating: string
 }
 
 export default class ResourceCreate extends Component<Props, ResourceCreateState>{
@@ -16,6 +24,13 @@ export default class ResourceCreate extends Component<Props, ResourceCreateState
         super(props)
         this.state = {
             title: '',
+            author: '',
+            link: '',
+            topic: '',
+            media: '',
+            readStatus: 'In my queue',
+            summary: '',
+            rating: '0'
         }
     }
 
@@ -29,17 +44,24 @@ export default class ResourceCreate extends Component<Props, ResourceCreateState
         >)
     }
 
-    // START HERE -- IT ISN"T GETTING TO HANDLESUBMIT 
     handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-        console.info ('In ResourceCreate handleSubmit')
+        console.info('In ResourceCreate handleSubmit ', this.state)
         e.preventDefault()
-        fetch('http://localhost:3000/book/', {
+        fetch('http://localhost:3000/book/create', {
             method: 'POST',
-            body: JSON.stringify({ title: this.state.title }),
+            body: JSON.stringify({
+                title: this.state.title,
+                author: this.state.author,
+                link: this.state.link,
+                topic: this.state.topic,
+                media: this.state.media,
+                readStatus: this.state.readStatus,
+                summary: this.state.summary,
+                rating: this.state.rating
+            }),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${this.props.token}`,
-
             }),
         })
             .then(res => res.json())
@@ -47,27 +69,71 @@ export default class ResourceCreate extends Component<Props, ResourceCreateState
                 console.log(bookData)
                 this.setState({
                     title: '',
+                    author: '',
+                    link: '',
+                    topic: '',
+                    media: '',
+                    readStatus: 'In my queue',
+                    summary: '',
+                    rating: '0'
                 })
                 this.props.fetchResources()
             })
     }
 
     render() {
-        console.info ('In ResourceCreate')
+        console.info('In ResourceCreate')
         return (
             <>
                 <h2>Enter Information about your Resource</h2>
-                <Form onSubmit={this.handleSubmit} />
-                <FormGroup>
-                    <Label htmlFor='title'>Title</Label>
-                    <Input
-                        name='title'
-                        value={this.state.title}
-                        onChange={this.handleChange}
-                    >
-                    </Input>
-                </FormGroup>
-                <Button type='submit'>Add Resource</Button>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label htmlFor='title'>Title</Label>
+                        <Input
+                            name='title'
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='author'>Author</Label>
+                        <Input
+                            name='author'
+                            value={this.state.author}
+                            onChange={this.handleChange}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='link'>Link</Label>
+                        <Input
+                            name='link'
+                            value={this.state.link}
+                            onChange={this.handleChange}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='topic'>Topic</Label>
+                        <Input
+                            name='topic'
+                            value={this.state.topic}
+                            onChange={this.handleChange}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='media'>Media</Label>
+                        <Input
+                            name='media'
+                            value={this.state.media}
+                            onChange={this.handleChange}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <Button type='submit'>Add Resource</Button>
+                </Form>
             </>
         )
     }
