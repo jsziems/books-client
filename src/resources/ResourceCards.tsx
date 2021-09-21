abdf5fd847fe82bf7871791be3d48de1d1b028f8
Janet Ziems <jsziems@gmail.com>
2021-09-21 11:01:56 -0400

    Added non-functioning modal to card for editing read status.

--- src/resources/ResourceCards.tsx
+++ src/resources/ResourceCards.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card, CardBody, CardFooter, CardGroup, CardImg, CardText, CardTitle } from 'reactstrap'
 // import styled from 'styled-components'
+import ReadStatusModal from './ReadStatusModal'
 
 import { Resource } from '../types'
 
@@ -20,28 +21,10 @@ export default class ResourceCards extends Component<Props, {}> {
     render() {
         return (
             <>
-                {/* <div className="row">
-                    <div className="col-sm-6">
-                        {this.props.resources.map(resource => {
-                            return (
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h5 className="card-title">resource.title</h5>
-                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            )
-                            })}
-                    </div>
-                </div> */}
-
-
                 <div className="cards-container">
-                    {/* <CardGroup > */}
-                        {this.props.resources.map(resource => {
-                            return (
-                                <div className='testing'>
+                    {this.props.resources.map(resource => {
+                        return (
+                            <div className='testing'>
                                 <Card >
                                     <CardImg variant="top" src="holder.js/100px160" />
                                     <CardBody>
@@ -50,16 +33,22 @@ export default class ResourceCards extends Component<Props, {}> {
                                             This is a wider card with supporting text below as a natural lead-in to
                                             additional content. This content is a little bit longer.
                                         </CardText>
+
+                                        <ReadStatusModal 
+                                            token={this.props.token} 
+                                            resource={resource} 
+                                            fetchResources={this.props.fetchResources}
+                                        />
+
                                     </CardBody>
                                     <CardFooter>
                                         <small className="text-muted">Last updated 3 mins ago</small>
                                     </CardFooter>
                                 </Card>
-                                </div>
-                            )
-                        })}
-                    {/* </CardGroup> */}
-                </div> 
+                            </div>
+                        )
+                    })}
+                </div>
             </>
         )
     }
--- src/resources/ResourceIndex.tsx
+++ src/resources/ResourceIndex.tsx
@@ -3,7 +3,7 @@ import { Col, Container, Row } from 'reactstrap'
 
 import ResourceCards from './ResourceCards'
 import ResourceCreate from './ResourceCreate'
-
+import ReadStatusModal from './ReadStatusModal'
 type Props = {
     token: string
 }
@@ -43,21 +43,22 @@ export default class ResourceIndex extends Component<Props, ResourceIndexState> 
         this.fetchResources()
     }
 
-render() {
-    return (
-        <Container>
-            <Row>
-                <Col md="3">
-                    <ResourceCreate fetchResources={this.fetchResources} token={this.props.token} />
-                </Col>
-                <Col md='9'>
-                    <ResourceCards 
-                        resources={this.state.resources}
-                        token={this.props.token} 
-                        fetchResources={this.fetchResources} />
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col md="3">
+                        <ResourceCreate fetchResources={this.fetchResources} token={this.props.token} />
+                    </Col>
+                    <Col md='9'>
+                        <ResourceCards
+                            resources={this.state.resources}
+                            token={this.props.token}
+                            fetchResources={this.fetchResources} />
+                    </Col>
+                </Row>
+                
+            </Container>
+        )
+    }
 }
